ff8d3b4b7517313b801df6e262b1e04675a91c18
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-02-28 15:04:57 +0530

    [Ankit Raj] Adding BoilerPlate Code For Employee PayRoll App

---
+++ src/test/java/com/bridgelabz/EmployeePayRollapp/EmployeePayRollappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.EmployeePayRollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayRollappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
